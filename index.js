9879a582e71bfa760f35fb16707c3cd7dabb059e
myuser <myemail>
2024-05-07 21:15:19 -0400

    c

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const fs = require('fs');
 const hostname = "127.0.0.1";
 const port = 3000;
 
+
+//crreate new file
+fs.writeFile('tiago.txt','code test', function(err){
+    if(err) throw err;
+console.log("the file was created!!")
+})
+
+//create new file or insert a new one after the existing one
+fs.appendFile('tiago.txt',"\nthis is a new file",(err)=>{
+    if (err) throw err;
+    console.log('add a new file')
+})
+
 const server = http.createServer((req,res) =>{
     if(req.url == '/tiago'){
         fs.readFile('index.html',function(err,data){
